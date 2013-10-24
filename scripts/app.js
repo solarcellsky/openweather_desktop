077df2f02576855c468f9e4e0b063a555c9d272a
solar <solarcellsky@gmail.com>
2013-10-24 16:50:38 +0800

    .

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,53 @@
 var time_zone = 1000 * (new Date().getTimezoneOffset())*(-60);
 
+var spinner = function(holderid, R1, R2, count, stroke_width, colour) {
+    var sectorsCount = count || 12,
+        color = colour || "#fff",
+        width = stroke_width || 15,
+        r1 = Math.min(R1, R2) || 35,
+        r2 = Math.max(R1, R2) || 60,
+        cx = r2 + width,
+        cy = r2 + width,
+        r = Raphael(holderid, r2 * 2 + width * 2, r2 * 2 + width * 2),
+
+        sectors = [],
+        opacity = [],
+        beta = 2 * Math.PI / sectorsCount,
+
+        pathParams = {
+            stroke: color,
+            "stroke-width": width,
+            "stroke-linecap": "round"
+        };
+
+    for (var i = 0; i < sectorsCount; i++) {
+        var alpha = beta * i - Math.PI / 2,
+            cos = Math.cos(alpha),
+            sin = Math.sin(alpha);
+        opacity[i] = 1 / sectorsCount * i;
+        sectors[i] = r.path([
+            ["M", cx + r1 * cos, cy + r1 * sin],
+            ["L", cx + r2 * cos, cy + r2 * sin]
+        ]).attr(pathParams);
+        if (color == "rainbow") {
+            sectors[i].attr("stroke", Raphael.getColor());
+        }
+    }
+    var tick;
+    (function ticker() {
+        opacity.unshift(opacity.pop());
+        for (var i = 0; i < sectorsCount; i++) {
+            sectors[i].attr("opacity", opacity[i]);
+        }
+        r.safari();
+        tick = setTimeout(ticker, 1000 / sectorsCount);
+    })();
+    return function() {
+        clearTimeout(tick);
+        r.remove();
+    };
+};
+
 var getDates = function(UNIX_timestamp) {
     var a = new Date(UNIX_timestamp * 1000);
     var months = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'];
@@ -218,6 +266,7 @@ var getTimes = function(timestamp) {
     };
 
 var weatherWidget = function() {
+    spinner('spinner', 120, 120, 16, 15, '#fff');
     var cnt = 6;
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(function(position) {
